f4129b17d5bf7c69c898cf8a142536e2e7d11818
IceXinShou <41276723+IceLeiYu@users.noreply.github.com>
2022-07-28 21:52:26 +0800

    Changes by WavJaby

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    host: '0.0.0.0',
-    port: 25567,
-  },
-  build: {
-    target: 'esnext',
-    outDir: '../'
-  },
+    plugins: [solidPlugin()],
+    server: {
+        host: '0.0.0.0',
+        port: 25567,
+    },
+    build: {
+        target: 'esnext',
+        outDir: '../Public',
+    },
+    experimental: {
+        renderBuiltUrl(filename, {hostType, type}) {
+            console.log("./Public/"+filename)
+            // if (type === 'public') {
+            //     return 'https://www.domain.com/' + filename
+            // } else if (path.extname(importer) === '.js') {
+            //     return {runtime: `window.__assetsPath(${JSON.stringify(filename)})`}
+            // } else {
+            //     return 'https://cdn.domain.com/assets/' + filename
+            // }
+            return './Public/' + filename;
+        }
+    },
 });
