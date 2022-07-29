d1c569015da9c2917bd60511f532033c10c8c64d
IceXinShou <41276723+IceLeiYu@users.noreply.github.com>
2022-07-29 12:22:41 +0800

    Init

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,20 +1,79 @@
 import {defineConfig} from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'path';
+import * as fs from 'fs'
+
+const projectName = 'WebClub';
+const outputRoot = 'docs';
+const dynamicImportFiles = [
+    './Page/data/1.html'
+];
 
 export default defineConfig({
-    plugins: [solidPlugin()],
+    plugins: [solidPlugin(), buildParser(dynamicImportFiles)],
     server: {
         host: '0.0.0.0',
         port: 25567,
     },
     build: {
         target: 'esnext',
-        outDir: '../Public',
+        minify: 'terser',
+        emptyOutDir: true,
+        outDir: path.join('..', outputRoot),
     },
     experimental: {
         renderBuiltUrl(filename, {hostType, type}) {
-            return 'https://iceleiyu.github.io/WebClub/Public/' + filename;
+            return path.join('/', projectName, filename).replace(/\\/g,'/');
         }
     },
-})
-;
+});
+
+function buildParser(dynamicImportFiles) {
+    return {
+        name: 'parser',
+        buildEnd() {
+            const rootFolder = path.join('../', outputRoot);
+            const assetsFolder = path.join(rootFolder, '/assets');
+            setTimeout(function () {
+                replace();
+                for (const i of dynamicImportFiles) {
+                    copyFolder(path.join(projectName, i), path.join(rootFolder, i));
+                }
+            }, 600);
+
+            function copyFolder(src, dest) {
+                const exists = fs.existsSync(src);
+                const stats = exists && fs.statSync(src);
+                const isDirectory = exists && stats.isDirectory();
+                if (isDirectory) {
+                    fs.mkdirSync(dest, {recursive: true});
+                    fs.readdirSync(src).forEach(function (childItemName) {
+                        fs.copyFileSync(
+                            path.join(src, childItemName),
+                            path.join(dest, childItemName),
+                            fs.constants.COPYFILE_FICLONE
+                        );
+                    });
+                } else {
+                    fs.mkdirSync(path.dirname(dest), {recursive: true});
+                    fs.copyFileSync(src, dest, fs.constants.COPYFILE_FICLONE);
+                }
+            }
+
+            function replace() {
+                fs.readdirSync(assetsFolder).forEach(file => {
+                    if (!file.endsWith('.js')) return;
+                    const fileName = path.join(assetsFolder, file);
+                    try {
+                        fs.readFile(fileName, "utf-8", (err, data) => {
+                            const out = data.replace(/(?!(\[,))"assets\//g, `"${projectName}/assets/`);
+                            fs.writeFileSync(fileName, out);
+                        });
+                    } catch (err) {
+                        console.error(err);
+                    }
+                });
+            }
+        }
+    }
+}
